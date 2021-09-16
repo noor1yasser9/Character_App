904f46e42f66a3425284e4a52823a6404e5cdbec
Noor  Yasser <mrh61037@gmail.com>
2021-09-16 18:34:01 +0300

    add Main and Pubspec.yaml

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final AuthMethods _authMethods = AuthMethods();
+
+  @override
+  Widget build(BuildContext context) {}
+}
