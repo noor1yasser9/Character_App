35eeaf3e326b5cd1611ba325674501ebd0e278d0
Noor  Yasser <mrh61037@gmail.com>
2021-09-18 21:45:49 +0300

    add Presentaation Screens widgets Reources remved and added main

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,15 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
+  const MyApp() : super();
+
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  final AuthMethods _authMethods = AuthMethods();
-
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
