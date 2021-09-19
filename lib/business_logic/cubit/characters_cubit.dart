2bfe435f0801f3a69a90ac9d6d825e01d16e373b
Noor  Yasser <mrh61037@gmail.com>
2021-09-19 14:34:19 +0300

    to call ui and convert data from future to main thread and others

--- lib/business_logic/cubit/characters_cubit.dart
+++ lib/business_logic/cubit/characters_cubit.dart
@@ -1,8 +1,22 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter_breaking/data/models/characters.dart';
+import 'package:flutter_breaking/data/repository/characters_repository.dart';
 import 'package:meta/meta.dart';
 
 part 'characters_state.dart';
 
 class CharactersCubit extends Cubit<CharactersState> {
-  CharactersCubit() : super(CharactersInitial());
+  final CharactersRepository repository;
+  late List<Character> listChar;
+
+  CharactersCubit(this.repository) : super(CharactersInitial());
+
+  List<Character> getAllCharacters() {
+    repository.getAllCharacters().then((value) {
+      emit(CharactersLoaded(value));
+      listChar = value;
+    });
+
+    return listChar;
+  }
 }
