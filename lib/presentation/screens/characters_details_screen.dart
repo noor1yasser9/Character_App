7feed4bfe33955825c56008f29c1a7de8e6bde9c
Noor  Yasser <mrh61037@gmail.com>
2021-09-21 22:51:32 +0300

    add library in pubspec.yaml for animation text add Color yellow in my colors display data in charcters details screen

--- lib/presentation/screens/characters_details_screen.dart
+++ lib/presentation/screens/characters_details_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_breaking/business_logic/cubit/characters_cubit.dart';
@@ -18,6 +21,51 @@ class CharactersDetailsScreen extends StatelessWidget {
       body: CustomScrollView(
         slivers: [
           buildSliverAppBar(),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                Container(
+                  margin: EdgeInsets.fromLTRB(14, 14, 14, 0),
+                  padding: EdgeInsets.all(8),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      characterInfo('Job : ', character.jobs.join(' / ')),
+                      buildDivider(315),
+                      characterInfo(
+                          'Appeared in : ', character.categoryForTwoSeries),
+                      buildDivider(250),
+                      characterInfo('Seasons : ', character.appearanceOfSeasons.join(' / ')),
+                      buildDivider(280),
+                      characterInfo('Status : ', character.statusIfDeadOrAlive),
+                      buildDivider(300),
+                      character.betterCallSaulAppearance.isEmpty
+                          ? Container()
+                          : characterInfo('Better Call Saul Seasons : ',
+                              character.betterCallSaulAppearance.join(" / ")),
+                      character.betterCallSaulAppearance.isEmpty
+                          ? Container()
+                          : buildDivider(150),
+                      characterInfo('Actor/Actress : ', character.acotrName),
+                      buildDivider(235),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      BlocBuilder<CharactersCubit, CharactersState>(
+                        builder: (context, state) {
+                          return checkIfQuotesAreLoaded(state);
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 500,
+                )
+              ],
+            ),
+          ),
         ],
       ),
     );
@@ -43,5 +91,87 @@ class CharactersDetailsScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  characterInfo(String title, String join) {
+    return RichText(
+      maxLines: 1,
+      overflow: TextOverflow.ellipsis,
+      text: TextSpan(
+        children: [
+          TextSpan(
+            text: title,
+            style: TextStyle(
+              color: MyColors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 18,
+            ),
+          ),
+          TextSpan(
+            text: join,
+            style: TextStyle(
+              color: MyColors.white,
+              fontSize: 16,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildDivider(double endIndent) {
+    return Divider(
+      height: 30,
+      endIndent: endIndent,
+      color: MyColors.yellow,
+      thickness: 2,
+    );
+  }
+
+  Widget checkIfQuotesAreLoaded(CharactersState state) {
+    if (state is QuotesLoaded) {
+      return displayRandomQuoteOrEmptySpace(state);
+    } else {
+      return showProgressIndicator();
+    }
+  }
+
+  Widget showProgressIndicator() {
+    return Center(
+      child: CircularProgressIndicator(
+        valueColor: AlwaysStoppedAnimation<Color>(MyColors.white),
+      ),
+    );
+  }
+
+  Widget displayRandomQuoteOrEmptySpace(QuotesLoaded state) {
+    var quotes = (state).quotes;
+    if (quotes.length != 0) {
+      int randomQuoteIndex = Random().nextInt(quotes.length - 1);
+      return Center(
+        child: DefaultTextStyle(
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 20,
+            color: MyColors.white,
+            shadows: [
+              Shadow(
+                blurRadius: 7,
+                color: MyColors.white,
+                offset: Offset(0, 0),
+              )
+            ],
+          ),
+          child: AnimatedTextKit(
+            repeatForever: true,
+            animatedTexts: [
+              FlickerAnimatedText(quotes[randomQuoteIndex].quote),
+            ],
+          ),
+        ),
+      );
+    } else {
+      return Container();
+    }
   }
 }
